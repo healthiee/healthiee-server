14fdda143db154db736e0b43c625116c1f7e756a
Jihoi-Kang <jihoi.kang@gmail.com>
2023-08-12 16:07:29 +0900

    init

---
+++ src/main/kotlin/healthiee/rest/HelloController.kt
@@ -0,0 +1,12 @@
+package healthiee.rest
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+
+    @GetMapping("/hello")
+    fun hello(): String = "hello"
+
+}
